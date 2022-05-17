dabef52e891071810d4c2f405fec012a764041ea
Paul Francis <paul@francis.com>
2022-05-17 08:30:12 +0200

    updates

--- diffixElmPaperAttacks/paulScripts/README.md
+++ diffixElmPaperAttacks/paulScripts/README.md
@@ -2,6 +2,10 @@ This directory contains scripts used to manage execution of rpyc on a cluster.
 
 The scripts are very specific to the clusters at MPI-SWS.
 
+Upon first login, do `. dokey.sh`
+
+If host list changes:
+
 mkdir .venv
 
 python3 -m venv ~/.venv
@@ -9,6 +13,8 @@ python3 -m venv ~/.venv
 source .venv/bin/activate
 
 python3 -m pip install rpyc
+
+mkdir /local/francis
 
 rpyc_classic.py
 
--- diffixElmPaperAttacks/paulScripts/build.py
+++ diffixElmPaperAttacks/paulScripts/build.py
@@ -9,6 +9,7 @@ hosts = [ 'pinky07',
 
 login = 'francis'
 key = 'id_rsa_root'
+localStorage = '/local/francis'
 
 # Build basic execution script
 # echo 'paul01'
@@ -18,3 +19,12 @@ with open('exall.sh', 'w') as f:
         f.write(f"echo '{host}'\n")
         f.write(f"ssh -i ~/.ssh/{key} {login}@{host} $1\n")
 os.system('chmod 777 exall.sh')
+
+# Build script to use when a new host is added
+with open('newHost.sh', 'w') as f:
+    f.write("./exall.sh 'mkdir .venv'\n")
+    f.write("./exall.sh 'python3 -m venv ~/.venv'")
+    f.write("./exall.sh 'source .venv/bin/activate'")
+    f.write("./exall.sh 'python3 -m pip install rpyc'")
+    f.write("./exall.sh 'mkdir /local/francis'")
+os.system('chmod 777 newHost.sh')
