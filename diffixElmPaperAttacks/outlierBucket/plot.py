3ffb889e00da03365744ca19c1939c9b5265603c
Paul Francis <paul@francis.com>
2022-06-09 08:15:43 +0200

    add distinct graph

--- diffixElmPaperAttacks/outlierBucket/plot.py
+++ diffixElmPaperAttacks/outlierBucket/plot.py
@@ -141,3 +141,38 @@ for shape in shapes:
     plt.gca().add_patch(shape)
 ax2.legend(title='Unknown Vals',loc='lower left', bbox_to_anchor=(0.7, 0.0), ncol=1)
 plt.savefig('worst-by-out-factor.png',bbox_inches='tight')
+
+with open('distinctBetaData.json', 'r') as f:
+    data = json.load(f)
+df = pd.DataFrame.from_dict(data)
+df['alphbet'] = df['alphbet'].apply(str)
+df['outParams'] = df['outParams'].apply(str)
+df = df.rename(columns={'numUnknownVals':'Unknown Vals'})
+df = df.rename(columns={'alphbet':'Alpha-Beta'})
+
+plt.figure(figsize=(6, 3))
+ax = sns.scatterplot(data=df, x="CR", y="CI",hue='Unknown Vals',style='Alpha-Beta',s=80)
+ax.set(xscale='log')
+params = {
+    'numBoxes':20,
+    'fromLeft':0.000008,
+    'toLeft':0.00008,
+    'fromBottom':0.45,
+    'toBottom':0.7,
+    'right':2.0,
+    'top':1.1,
+    'alpha':0.03,
+    'risk_level':'low',
+    #'alpha':0.5,
+}
+rp = tools.risk.riskPatches()
+shapes = rp.getShapes(params)
+plt.xlabel('Prediction Rate (PR)',fontsize=12)
+plt.ylabel('Precision Improvement (PI)',fontsize=12)
+ax.legend(loc='lower center', bbox_to_anchor=(0.5, 1.0), ncol=2)
+plt.grid()
+plt.ylim(0,1.0)
+plt.xlim(0.000001,1.2)
+for shape in shapes:
+    plt.gca().add_patch(shape)
+plt.savefig('distinct-beta-outlier.png',bbox_inches='tight')
